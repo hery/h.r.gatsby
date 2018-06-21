30397a95ebb490f7ce9ce7cc2332e6b9d6a46a62
Hery Ratsimihah <hery@careerjet.com>
2018-06-21 14:35:56 +0100

    Added layouts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
-import React from "react"
 
-export default () => <div>Hello world!</div>
+
+import React from "react";
+
+export default () => (
+    <div>
+        <h1>Hai!</h1>
+        <p>Der!</p>
+    </div>
+);
