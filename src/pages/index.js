b743c7e1ea858cc565c8354d85037b70d63afc1a
Hery Ratsimihah <hery@careerjet.com>
2018-06-22 17:14:52 +0100

    Added site metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,19 @@
 
-
 import React from "react";
 
-export default () => (
+export default ({ data }) => (
     <div>
-        <h1>Hai!</h1>
+        <h1>Hai { data.site.siteMetadata.title }!</h1>
         <p>Der!</p>
     </div>
 );
+
+export const query = graphql`
+    query IndexQuery {
+        site {
+            siteMetadata {
+                title
+            }
+        }
+    }
+`
